b32892586fce3c9254b40b7b259609cb4eaecbee
Ramil Bagautdinov <ra777@bk.ru>
2022-06-06 20:34:26 +0300

    2.17 Need more function

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getRandomInt(min, max) {
+  if (min >= max) {
+    throw new Error('min cannot be greater than or equal to max');
+  }
+  if (min < 0 || max < 0) {
+    throw new Error('min and max cannot be less then 0');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt();
+
+function checkStringLength (value, max) {
+  value = String(value);
+  return value.length <= max ;
+}
+
+checkStringLength();
